a67e25940f35104254d305cc382a96a8819f7491
Vinícius <vini.sribeiro@hotmail.com>
2025-02-03 23:15:16 -0300

    GameMinDTO, GameRepository, GameService, GameController

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -17,22 +17,24 @@ public class Game {
     private String platforms;
     private Double score;
     private String imgUrl;
-    @Column(columnDefinition = "TEXT")
     private String shortDescription;
-
     @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {super();
-        this.id = id;
-        this.title = title;
-        this.year = year;
-        Genre = genre;
-        this.platforms = platforms;
-        this.score = score;
-        this.imgUrl = imgUrl;
-        this.shortDescription = shortDescription;
-        this.longDescription = longDescription;
+        id = id;
+        title = title;
+        year = year;
+        genre = genre;
+        platforms = platforms;
+        score = score;
+        imgUrl = imgUrl;
+        shortDescription = shortDescription;
+        longDescription = longDescription;
+    }
+
+    public Game() {
+
     }
 
     public Long getId() {
